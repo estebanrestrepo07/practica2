7dbb2b0e750af3728b811e4ebfbca64e2073e896
santiago campuzano <tiago150995@hotmail.com>
2017-05-21 18:26:25 -0500

    Borrado de cosas innecesarias

--- Practica2/src/modelo/GrafoG.java
+++ Practica2/src/modelo/GrafoG.java
@@ -62,29 +62,6 @@ public class GrafoG {
                aggArista(vb,va);
            }
        }
-//       NodoG a,b,aux = null; //Nuevos nodos, les doy el valor en campo dato los enteros que entraron por parámetro.
-//       a = new NodoG(va, null);
-//       b = new NodoG(vb, null);
-//       
-//       //Inserción de b como vecino de a
-//       if(vecinos[va].getLiga()==null){
-//           vecinos[va].setLiga(b);
-//           aux=a;
-//       }
-//       else{
-//          vecinos[va].setLiga(b);
-//          a.setLiga(aux);
-//       }
-//       
-//       //Inserción de a como vecino de b
-//       if(vecinos[vb].getLiga()==null){
-//           vecinos[vb].setLiga(a);
-//           aux=b;
-//       }
-//       else{
-//          vecinos[vb].setLiga(a);
-//          b.setLiga(aux);
-//       }
     }
     
     public void borraArista(int va,int vb){
